2d07f7b3891ca0457df5259af602f4922f918fbe
nathan mimoun <nathan.mimoun@gmail.com>
2020-01-29 00:51:33 +0100

    display & add  favorite function added

--- project8/addfav/views.py
+++ project8/addfav/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from offapi.models import ProductSaved, Product
 from authentification.models import User
 
@@ -6,10 +6,19 @@ from authentification.models import User
 def add(request):
     return render(request, 'addfav/add.html')
 
-def favorite(request):
+def favorite(request, pk, pk2):
     b = ProductSaved(user_id = User.objects.get(username='admin'),
-                    product_id = Product.objects.get(product_id = 5449000000996),
-                    sub_id = Product.objects.get(product_id = 3068320114453))
+                    product_id = Product.objects.get(product_id = pk),
+                    sub_id = Product.objects.get(product_id = pk2))
     b.save()
 
-    return render(request, 'addfav/add.html')
+    return redirect('/') # doit pouvoir rediriger vers la page d'origine et non la page d'acceuil
+
+def display_favorite(request):
+    user = request.user.email #doit pouvoir recevoir le nom de l'utilisateur
+    p = ProductSaved.objects.all().filter(user_id= User.objects.get(email=user))
+    context = {'products': p,
+                'title': 'favorite'
+                }   
+
+    return render(request, 'addfav/display.html', context)
