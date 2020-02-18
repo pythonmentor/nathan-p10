26d4b5e3ad5cfd38c1b40f6898de03f1b5c6a386
nathan mimoun <nathan.mimoun@gmail.com>
2020-02-18 14:04:26 +0100

    change debug to false and added static root

--- project8/settings.py
+++ project8/settings.py
@@ -127,10 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
@@ -138,5 +140,5 @@ LOGIN_REDIRECT_URL = 'home'
 
 AUTH_USER_MODEL = 'authentification.User'
 
-# Activate Django-Heroku.
+# # Activate Django-Heroku.
 django_heroku.settings(locals())
