b52427e675313504b6566688db8c8b00805daeac
nathan mimoun <nathan.mimoun@gmail.com>
2020-03-09 15:19:18 +0100

    Pagination fonction added

--- search/views.py
+++ search/views.py
@@ -1,6 +1,7 @@
 import logging
 from sentry_sdk import capture_message
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from .forms import SearchForm
 from offapi.models import Product, Category
 from django.views.generic import FormView, ListView
@@ -12,8 +13,11 @@ logger = logging.getLogger(__name__)
 
 def search(request):
     query = request.GET.get('q')
-    queryset = Product.objects.filter(product_name__icontains=query)[:12]
-    context = {'products': queryset,
+    queryset = Product.objects.filter(product_name__icontains=query).order_by('product_name')
+    paginator = Paginator(queryset, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'products': page_obj,
                'title': 'Recherche',
                'big_title': 'voici le resultat de votre recherche'}
 
